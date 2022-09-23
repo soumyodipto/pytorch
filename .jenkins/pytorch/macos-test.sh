5b918e303756395818c0b9067f26f7768285757b
Jane Xu <janeyx@fb.com>
2022-09-23 21:50:21 +0000

    Add opt-einsum to CI + make it a hard requirement

--- .jenkins/pytorch/macos-test.sh
+++ .jenkins/pytorch/macos-test.sh
@@ -6,7 +6,7 @@ source "$(dirname "${BASH_SOURCE[0]}")/macos-common.sh"
 
 conda install -y six
 if [[ ${BUILD_ENVIRONMENT} = *arm64* ]]; then
-  pip install hypothesis "expecttest==0.1.3" "librosa>=0.6.2" "numba==0.56.0" psutil "scipy==1.9.0"
+  pip >=0.6.2" "numba==0.56.0" psutil "scipy==1.9.0"
 else
   pip install hypothesis "expecttest==0.1.3" "librosa>=0.6.2" "numba<=0.49.1" psutil "scipy==1.6.3"
 fi
@@ -19,7 +19,8 @@ pip install "unittest-xml-reporting<=3.2.0,>=2.0.0" \
   pytest-shard \
   pytest-rerunfailures \
   "xdoctest==1.0.2" \
-  "pygments==2.12.0"
+  "pygments==2.12.0" \
+  "opt-einsum>=3.3"
 
 if [ -z "${CI}" ]; then
   rm -rf "${WORKSPACE_DIR}"/miniconda3/lib/python3.6/site-packages/torch*
